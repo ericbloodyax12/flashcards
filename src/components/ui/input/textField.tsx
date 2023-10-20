2b01539d229bf6f22ac1184a66935108cb401935
Young Eric <erikbonopart12@gmail.com>
2023-10-20 13:32:42 +0300

    -add Eyes icons component and customizing styles fot them and add logic show password and off

--- src/components/ui/input/textField.tsx
+++ src/components/ui/input/textField.tsx
@@ -1,26 +1,41 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, forwardRef, useState } from 'react'
 
-import { SearchIcon } from '@/assets/iconComponents/search.tsx'
+import { EyeIcon } from '@/assets/iconComponents/eye'
+import { EyeOffIcon } from '@/assets/iconComponents/eyeoff'
+import { SearchIcon } from '@/assets/iconComponents/search'
 
 import s from './input.module.scss'
-// todo:  a) error logic б) icons c) label d) err messag
+// todo:  a) icon for password
 export type InputProps = {
   errorMessage?: string
   fullWidth?: boolean
+  search?: boolean
   variant?: 'default' | 'password' | 'search'
 } & ComponentPropsWithoutRef<'input'>
 
 export const TextField = forwardRef<HTMLInputElement, InputProps>(props => {
-  const { className, errorMessage, fullWidth, variant = 'default', ...rest } = props
+  const { className, errorMessage, fullWidth, search, variant = 'default', ...rest } = props
+  const [showPassword, setShowPassword] = useState(false)
 
   return (
     <div className={s.inputContainer}>
-      <SearchIcon className={s.icon} />
+      {variant === 'search' && <SearchIcon className={s.icon} />}
+
       <input
         className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
         type={variant}
         {...rest}
       />
+      <button
+        onClick={() => {
+          setShowPassword(value => !value)
+        }}
+        style={{ all: 'unset', backgroundColor: 'transparent' }}
+      >
+        {showPassword
+          ? variant === 'password' && <EyeOffIcon className={s.eyeIconOff} />
+          : variant === 'password' && <EyeIcon className={s.eyeIcon} />}
+      </button>
     </div>
 
     // <Typography className={classNames.error} variant={'error'}>
