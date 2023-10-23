cb4f5807d6e66eb9de25ab7a50154ce15b50ede8
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-10-23 11:18:21 +0600

    -add header and scss styles

---
+++ src/components/ui/header/header.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+
+import logo from '@/assets/images/Logo.svg'
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
+
+import s from './header.module.scss'
+export type Props = {
+  className?: string
+  username?: string
+}
+export const Header: FC<Props> = ({ className, username }) => {
+  return (
+    <header className={`${s.header} ${className ? className : ''}`}>
+      <div className={`container ${s.wrapper}`}>
+        <a className={s.logo} href={'#'}>
+          <img alt={'logo'} height={36} src={logo} width={156} />
+        </a>
+        {username ? (
+          <span className={s.userInfo}>
+            {username && (
+              <Typography as={'span'} className={s.username} variant={'subtitle1'}>
+                {username}
+              </Typography>
+            )}
+          </span>
+        ) : (
+          <Button>{'Sign In'}</Button>
+        )}
+      </div>
+    </header>
+  )
+}
