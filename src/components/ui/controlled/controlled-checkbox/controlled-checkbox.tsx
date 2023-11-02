21319536488f24701b675d0e313f0d96ce16a8ef
h12t20 <sergdiamond@ymail.com>
2023-11-02 11:33:27 +0300

    add controlled-checkbox component

---
+++ src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -0,0 +1,16 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { Checkbox, CheckboxProps } from '@/components/ui/checkbox'
+
+type Props<T extends FieldValues> = UseControllerProps<T> &
+  Omit<CheckboxProps, 'checked' | 'onCheckedChange'>
+export const ControlledCheckbox = <T extends FieldValues>({ control, name, ...rest }: Props<T>) => {
+  const {
+    field: { onChange, value, ...field },
+  } = useController({
+    control,
+    name,
+  })
+
+  return <Checkbox {...rest} {...field} checked={value} onCheckedChange={onChange} />
+}
