928f348d704163272894a4746f24e5406e9a9e5a
Omurbekova Tolkun <tolkun3922@gmail.com>
2023-11-07 13:36:12 +0600

    -add missing forwardRef

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, forwardRef } from 'react'
 
 import logo from '@/assets/images/Logo.svg'
 import { Button } from '@/components/ui/button'
@@ -9,25 +9,27 @@ export type Props = {
   className?: string
   username?: string
 }
-export const Header: FC<Props> = ({ className, username }) => {
-  return (
-    <header className={`${s.header} ${className ? className : ''}`}>
-      <div className={`container ${s.wrapper}`}>
-        <a className={s.logo} href={'#'}>
-          <img alt={'logo'} height={36} src={logo} width={156} />
-        </a>
-        {username ? (
-          <span className={s.userInfo}>
-            {username && (
-              <Typography as={'span'} className={s.username} variant={'subtitle1'}>
-                {username}
-              </Typography>
-            )}
-          </span>
-        ) : (
-          <Button>{'Sign In'}</Button>
-        )}
-      </div>
-    </header>
-  )
-}
+export const Header: FC<Props> = forwardRef<HTMLHeadElement, Props>(
+  ({ className, username }, ref) => {
+    return (
+      <header className={`${s.header} ${className ? className : ''}`} ref={ref}>
+        <div className={`container ${s.wrapper}`}>
+          <a className={s.logo} href={'#'}>
+            <img alt={'logo'} height={36} src={logo} width={156} />
+          </a>
+          {username ? (
+            <span className={s.userInfo}>
+              {username && (
+                <Typography as={'span'} className={s.username} variant={'subtitle1'}>
+                  {username}
+                </Typography>
+              )}
+            </span>
+          ) : (
+            <Button>{'Sign In'}</Button>
+          )}
+        </div>
+      </header>
+    )
+  }
+)
