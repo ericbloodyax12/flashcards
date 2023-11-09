d4403bed51d92d1b94d779898baffba4613735ec
h12t20 <sergdiamond@ymail.com>
2023-11-09 23:23:00 +0300

    changed select

--- src/components/ui/select/select.stories.tsx
+++ src/components/ui/select/select.stories.tsx
@@ -12,7 +12,7 @@ const meta = {
   title: 'Components/Select',
 } satisfies Meta<typeof Select>
 
-const selectArgs = {
+const args = {
   disabled: false,
   options: [
     { label: 'Select-box1', value: 'Select-box1' },
@@ -27,10 +27,12 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const SelectDefault = () => <Select {...selectArgs} />
+export const SelectDefault: Story = {
+  args,
+}
 
 export const SelectPrimary: Story = {
-  args: { ...selectArgs, className: s.primary },
+  args: { ...args, className: s.primary },
 }
 
 const select = clsx(s.customized, s.margin)
@@ -38,8 +40,8 @@ const select = clsx(s.customized, s.margin)
 export const SelectCustomized = () => {
   return (
     <>
-      <Select className={select} defaultValue={selectArgs.options[0].value} title={'Select-box'}>
-        {selectArgs.options.map(option => (
+      <Select className={select} defaultValue={args.options[0].value} title={'Select-box'}>
+        {args.options.map(option => (
           <SelectItem className={s.customized} key={option.value} value={option.value}>
             <Typography variant={'h2'}>{option.label}</Typography>
           </SelectItem>
